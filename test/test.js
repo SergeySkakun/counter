9c2197a4c9b82ae2bcfc0077931a95395ebd6f9a
SergeySkakun <thexfiles116@gmail.com>
2018-11-05 12:50:41 +0300

    feat: add tests for 'increase' method of class Counter

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 const assert = require('assert');
 const testingClass = require("../counter");
-console.log(require("../counter"));
 
 describe("Test class Counter.", function() {
     describe("Test constructor", function(){
@@ -20,6 +19,50 @@ describe("Test class Counter.", function() {
             let newInstanceClass = new testingClass(-1, -1);
             assert.equal(newInstanceClass.value, -1);
             assert.equal(newInstanceClass.step, -1);
+        });
+    });
+
+    describe("Test 'increase' method.", function() {
+        it("One call 'increase' method with positive pararmeters", function() {
+            let newInstanceClass = new testingClass(1, 1);
+            newInstanceClass.increase();
+            assert.equal(newInstanceClass.value, 2);
+        });
+
+        it("Multiple call 'increase' method with positive pararmeters", function() {
+            let newInstanceClass = new testingClass(2, 2);
+            newInstanceClass.increase();
+            newInstanceClass.increase();
+            newInstanceClass.increase();
+            newInstanceClass.increase();
+            newInstanceClass.increase();
+            assert.equal(newInstanceClass.value, 12);
+        });
+
+        it("One call 'increase' method with 0 in all parametrs", function() {
+            let newInstanceClass = new testingClass(0, 0);
+            newInstanceClass.increase();
+            assert.equal(newInstanceClass.value, 0);
+        });
+
+        it("Multiple call 'increase' method with step = 0", function() {
+            let newInstanceClass = new testingClass(5, 0);
+            newInstanceClass.increase();
+            newInstanceClass.increase();
+            newInstanceClass.increase();
+            newInstanceClass.increase();
+            newInstanceClass.increase();
+            assert.equal(newInstanceClass.value, 5);
+        });
+
+        it("Multiple call 'increase' method with  with negative pararmeters", function() {
+            let newInstanceClass = new testingClass(5, -5);
+            newInstanceClass.increase();
+            newInstanceClass.increase();
+            newInstanceClass.increase();
+            newInstanceClass.increase();
+            newInstanceClass.increase();
+            assert.equal(newInstanceClass.value, -20);
         });
     });
 });
